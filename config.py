1adf4e0c8845fefaf0225660fb00399b738a2e37
Merbau Alexandr <merbau.alex@gmail.com>
2024-10-06 02:53:46 +0700

    обновил логику

--- config.py
+++ config.py
@@ -1,5 +1,28 @@
-iam_token = "ваш iam токен"
-catalog_id = "ваш id каталога"
+# C:\Users\Pavlu4ini\PycharmProjects\PE31_ZeroLogos\config.py
+import os
+from dotenv import load_dotenv
+import requests
+
+# Загружаем переменные из .env
+load_dotenv()
+
+# Получаем OAuth-токен из .env
+oauth_token = os.getenv('OAUTH_TOKEN')
+
+# Функция для получения IAM-токена
+def get_iam_token():
+    url = "https://iam.api.cloud.yandex.net/iam/v1/tokens"
+    data = {
+        "yandexPassportOauthToken": oauth_token
+    }
+    response = requests.post(url, json=data)
+
+    if response.status_code == 200:
+        return response.json()['iamToken']
+    else:
+        raise Exception(f"Ошибка: {response.status_code} - {response.text}")
+
+# Остальные параметры
+catalog_id = "b1gnm09h06o5ufr781h7"
 url_1 = "https://llm.api.cloud.yandex.net/foundationModels/v1/imageGenerationAsync"
 url_2 = "https://llm.api.cloud.yandex.net:443/operations"
-
--- logo_gen.py
+++ logo_gen.py
@@ -1,3 +1,4 @@
+# C:\Users\Pavlu4ini\PycharmProjects\PE31_ZeroLogos\logo_gen.py
 import requests
 import random
 import time
@@ -5,11 +6,18 @@ import base64
 import os
 import config as cfg
 
+
 def generate_logo(forma, style, description):
+    # Получаем IAM-токен
+    iam_token = cfg.get_iam_token()
+
+    # Настраиваем заголовки с актуальным IAM-токеном
     headers = {
-        "Authorization": f"Bearer {cfg.iam_token}",
+        "Authorization": f"Bearer {iam_token}",
         "Content-Type": "application/json"
     }
+
+    # Данные для запроса
     data = {
         "modelUri": f"art://{cfg.catalog_id}/yandex-art/latest",
         "generationOptions": {
@@ -17,7 +25,7 @@ def generate_logo(forma, style, description):
             "aspectRatio": {
                 "widthRatio": "1",
                 "heightRatio": "1"
-           }
+            }
         },
         "messages": [
             {
@@ -26,16 +34,22 @@ def generate_logo(forma, style, description):
             }
         ]
     }
+
+    # Отправляем POST-запрос на генерацию изображения
     response = requests.post(cfg.url_1, headers=headers, json=data)
     if response.status_code == 200:
         request_id = response.json()['id']
-        time.sleep(20)
+        time.sleep(20)  # Ожидание обработки запроса
         headers.pop("Content-Type")
+
+        # Отправляем GET-запрос для получения результата
         response = requests.get(f"{cfg.url_2}/{request_id}", headers=headers)
         if response.status_code == 200:
             image_base64 = response.json()['response']['image']
             image_data = base64.b64decode(image_base64)
             image_path = os.path.join('static', 'image.jpeg')
+
+            # Сохраняем изображение
             with open(image_path, 'wb') as file:
                 file.write(image_data)
             return image_path
